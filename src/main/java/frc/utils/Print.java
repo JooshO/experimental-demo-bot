b68c8fb07e3c65719090780982d7706c2672af11
Josh Overbeek <joshoverbeek0@gmail.com>
2022-07-08 13:13:31 -0400

    Add documentation to Print class

--- src/main/java/frc/utils/Print.java
+++ src/main/java/frc/utils/Print.java
@@ -6,10 +6,19 @@ package frc.utils;
  * @author Josh Overbeek
  */
 public class Print {
+    /**
+     * Printline but only prints in debug
+     * @param str String to print
+     */
     public synchronized static void dPrintLn(String str) {
         if (Debug.isDebug()) System.out.println(str);
     }
 
+    /**
+     * Printf but only in debug
+     * @param format String formatting
+     * @param args Arguements to put in the formatting
+     */
     public synchronized static void dPrintF(String format, Object... args) {
         if (Debug.isDebug()) System.out.printf(format, args);
     }
